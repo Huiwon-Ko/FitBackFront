0f5a74a35fe1e8aa86faf3c067ae79d74bee3cb2
나 <khw7473@sookmyung.ac.kr>
2022-11-24 22:15:06 +0900

    Folder Change

--- react_login_form-main/src/components/GoogleButton.js
+++ react_login_form-main/src/components/GoogleButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
-import axios from 'axios'
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import axios from 'axios' 
+/*import GoogleLogin from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google'; */
 
 
 function GoogleButton(){
-    const clientId = "372495721999-tkdujncr3rjq27huenf9en6ja1v48tid.apps.googleusercontent.com"
+    const clientId = '372495721999-tkdujncr3rjq27huenf9en6ja1v48tid.apps.googleusercontent.com';
 
     
     const onSuccess = async(response) => {
@@ -32,14 +34,56 @@ function GoogleButton(){
 
     return(
         <div>
-            <GoogleLogin
-                clientId={clientId}
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                cookiePolicy="single_host_origin"
-            />
+            <GoogleOAuthProvider clientId={clientId}>
+                <GoogleLogin
+                    clientId={clientId}
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy="single_host_origin"
+                />
+            </GoogleOAuthProvider>
         </div>
     )
 }
 
-export default GoogleButton
+export default GoogleButton 
+
+/*
+import React, { useCallback, useEffect } from "react";
+import GoogleLogin from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+const clientId = '372495721999-tkdujncr3rjq27huenf9en6ja1v48tid.apps.googleusercontent.com'
+
+const GoogleButton = ({ onSocial }) => {
+    useEffect(() => {
+        function start() {
+            gapi.client.init({
+                clientId,
+                scope: 'email',
+            });
+        }
+
+        gapi.load('clent:auth2', start);
+    }, []);
+
+    const onSuccess = (response) => {
+        console.log(response);
+    };
+
+    const onFailure = (response) => {
+        console.log(response);
+    };
+
+    return (
+        <div>
+            <GoogleLogin
+                clientId={clientId}
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+            />
+        </div>
+    );
+};
+
+export default GoogleButton; */
